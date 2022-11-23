d279140ecc60b4b3862f849972d8c44172058a0f
Ryan Parker <parkerzr@amazon.com>
2022-11-22 16:49:06 -0800

    swap to yarn and bump deps

--- main.ts
+++ main.ts
@@ -3,10 +3,10 @@ import { App, Chart, ChartProps } from 'cdk8s';
 import { Deployment, HorizontalPodAutoscaler, Volume } from 'cdk8s-plus-25';
 
 export class MyChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
     super(scope, id, props);
     const deployment = new Deployment(this, 'deployment');
-    const volumeMounts = [{ volume: Volume.fromEmptyDir(deployment, 'test-volume','test'), path: './test'}];
+    const volumeMounts = [{ volume: Volume.fromEmptyDir(deployment, 'test-volume', 'test'), path: './test' }];
 
     deployment.addContainer({
       image: 'ubuntu',
